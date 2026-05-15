39fe824480942d9742d6d283b8e1a47321babd67
Miguelmdcc <miguelmendacic123@gmail.com>
2026-05-15 07:47:15 -0300

    feat: LB-1 initiating java project

---
+++ src/test/java/com/example/lance_bovino/LanceBovinoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lance_bovino;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LanceBovinoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
