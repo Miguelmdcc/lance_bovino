46971f689ed8c6cc3fa3f4fe2bb52e2530260a18
Miguelmdcc <miguelmendacic123@gmail.com>
2026-06-26 07:32:16 -0300

    fix: main cpf string

--- src/main/resources/db/migration/V01__cria_tabela_usuario.sql
+++ src/main/resources/db/migration/V01__cria_tabela_usuario.sql
@@ -13,8 +13,8 @@ CREATE TABLE public.usuario
 );
 
 INSERT INTO usuario (nome, cpf, metodo_bancario, dados_bancarios, senha) VALUES
-('Teste ADM', 752.217.790-13, 'PIX','450cvv','{noop}12345'),
-('Teste USER', 383.868.940-24, 'PIX','450cvv','{noop}12345'),
-('Teste USEReADM', 259.122.000-00, 'PIX','450cvv','{noop}12345');
+('Teste ADM', "752.217.790-13", 'PIX','450cvv','{noop}12345'),
+('Teste USER', "383.868.940-24", 'PIX','450cvv','{noop}12345'),
+('Teste USEReADM', "259.122.000-00", 'PIX','450cvv','{noop}12345');
 
 END;
